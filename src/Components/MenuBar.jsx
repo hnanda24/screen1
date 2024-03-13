5690ffc468aafdf141e62634b25f2b7818059141
Hardik <hnanda005@gmail.com>
2024-03-13 16:34:27 +0530

    first commit

---
+++ src/Components/MenuBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const MenuBar = () => {
+  return (
+    <div>
+        <Nav clasName ='w-[20vw]  '>
+            <div>
+                <img src='/Vector 3.png'/>
+            </div>
+        </Nav>
+    </div>
+  )
+}
+
+export default MenuBar
